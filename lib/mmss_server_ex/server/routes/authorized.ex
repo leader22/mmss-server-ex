22fc8472a1190fc977800ef0e4bee6b85d9305aa
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-12-22 09:45:13 +0900

    Fix auth resp

--- lib/mmss_server_ex/server/routes/authorized.ex
+++ lib/mmss_server_ex/server/routes/authorized.ex
@@ -3,9 +3,7 @@ defmodule MMSSServer.Routes.Authorized do
 
   def get_session(conn) do
     if !login?(conn) do
-      conn
-      |> put_resp_content_type("application/json")
-      |> send_resp(401, Poison.encode!(%{error: 3}))
+      unauthorized(conn)
     end
 
     conn
@@ -14,24 +12,29 @@ defmodule MMSSServer.Routes.Authorized do
   end
 
   def get_track(conn) do
+    if !login?(conn) do
+      unauthorized(conn)
+    end
+
     conn = fetch_query_params(conn)
     path = conn.params["path"]
     IO.inspect(path)
-
-    if !login?(conn) do
-      conn
-      |> put_resp_content_type("application/json")
-      |> send_resp(401, Poison.encode!(%{error: 3}))
-    end
 
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Poison.encode!(nil))
   end
 
-  defp login?(conn) do
+  defp unauthorized(conn) do
     conn
-    |> fetch_session()
-    |> get_session(:isLogin)
+    |> put_resp_content_type("application/json")
+    |> send_resp(401, Poison.encode!(%{error: 3}))
+  end
+
+  defp login?(conn) do
+    nil !=
+      conn
+      |> fetch_session()
+      |> get_session(:isLogin)
   end
 end
